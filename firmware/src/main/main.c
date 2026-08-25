773293fb5ccd09f2bda1f46a77457623be9e597b
ilfpns <yeomsh09@gmail.com>
2026-08-25 23:49:14 +0900

    Add led blink logic

--- firmware/src/main/main.c
+++ firmware/src/main/main.c
@@ -1,9 +1,18 @@
 #include "stm32f1xx.h"
 #include "../clock/clock_setting.h"
 
-int main(void) {
-    APB2EN_CLOCK;
-    while (1) {
+#include <stdint.h>
+#include <stdio.h>
 
+int main(void) {
+    set_the_clock();
+    printf("clock settings succeful");
+
+    for (;;) {
+        GPIOA->BSRR = GPIO_BSRR_BS5;
+        for (volatile uint32_t i = 0; i < 500000; i++) {}
+
+        GPIOA->BSRR = GPIO_BSRR_BR5;
+        for (volatile uint32_t i = 0; i < 500000; i++) {}
     }
 }
