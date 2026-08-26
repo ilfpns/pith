b32ea92646cac31d28903b19fb275fb8ace61f3d
ilfpns <yeomsh09@gmail.com>
2026-08-26 17:35:57 +0900

    Add button control logic

--- firmware/src/clock/clock_setting.h
+++ firmware/src/clock/clock_setting.h
@@ -1,8 +1,8 @@
 #include "stm32f1xx.h"
 
 // Prevent redefinition
-#undef  APB2EN_CLOCK
 #undef  RCC_APB2ENR_IOPAEN
+#undef  RCC_APB2ENR_IOPCEN
 
 #undef GPIO_CRL_CNF5
 #undef GPIO_CRL_MODE5
@@ -11,21 +11,42 @@
 #undef GPIO_BSRR_BS5
 #undef GPIO_BSRR_BR5
 
+#undef GPIO_CRH_CNF13
+#undef GPIO_CRH_CNF13_1
+#undef GPIO_CRH_MODE13
+
+#undef GPIO_IDR_IDR13
+#undef GPIO_ODR_ODR13
+
 // Pin Clock
 #define GPIO_CRL_CNF5 (0x3UL << 22)
 #define GPIO_CRL_MODE5 (0x3UL << 20)
 #define GPIO_CRL_MODE5_0 (0x1UL << 20)
 
+#define GPIO_CRH_CNF13 (0x3UL << 22)
+#define GPIO_CRH_CNF13_1 (0x2UL << 22)
+#define GPIO_CRH_MODE13 (0x3UL << 20)
+
 // Init peripheral register
 #define  RCC_APB2ENR_IOPAEN (1UL << 2)
+#define  RCC_APB2ENR_IOPCEN (1UL << 4)
 
 // Bsrr configuration
 #define GPIO_BSRR_BS5 (1UL << 5)
 #define GPIO_BSRR_BR5 (1UL << 21)
 
+// IDR & ODR
+#define GPIO_IDR_IDR13 (1UL << 13)
+#define GPIO_ODR_ODR13 (1UL << 13)
+
 void set_the_clock() {
     RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
 
     GPIOA->CRL &= ~(GPIO_CRL_CNF5 | GPIO_CRL_MODE5);
     GPIOA->CRL |= GPIO_CRL_MODE5_0;
+
+    GPIOC->CRL &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13);
+    GPIOC->CRL |= GPIO_CRH_CNF13_1;
+    GPIOC->CRL |= GPIO_ODR_ODR13;
 }
--- firmware/src/main/main.c
+++ firmware/src/main/main.c
@@ -1,6 +1,8 @@
+#include "stm32f103xb.h"
 #include "stm32f1xx.h"
 #include "../clock/clock_setting.h"
 
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -14,5 +16,11 @@ int main(void) {
 
         GPIOA->BSRR = GPIO_BSRR_BR5;
         for (volatile uint32_t i = 0; i < 500000; i++) {}
+
+        if ((GPIOC->IDR & GPIO_IDR_IDR13) == 0) {
+            printf("1");
+        } else {
+            printf("0");
+        }
     }
 }
