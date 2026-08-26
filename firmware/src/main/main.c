4b9f760aac37cdb3031552fe417bfaf82d91a5c9
ilfpns <yeomsh09@gmail.com>
2026-08-27 00:19:24 +0900

    fix linker

--- firmware/src/main/main.c
+++ firmware/src/main/main.c
@@ -10,17 +10,15 @@ int main(void) {
     set_the_clock();
     printf("clock settings succeful");
 
+    GPIOA->BSRR = GPIO_BSRR_BR5;
+
     for (;;) {
-        GPIOA->BSRR = GPIO_BSRR_BS5;
-        for (volatile uint32_t i = 0; i < 500000; i++) {}
-
-        GPIOA->BSRR = GPIO_BSRR_BR5;
-        for (volatile uint32_t i = 0; i < 500000; i++) {}
-
         if ((GPIOC->IDR & GPIO_IDR_IDR13) == 0) {
             printf("1");
+            GPIOA->BSRR = GPIO_BSRR_BS5;
         } else {
             printf("0");
+            GPIOA->BSRR = GPIO_BSRR_BR5;
         }
     }
 }
