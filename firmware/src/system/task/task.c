7fd74a059a65ca8fb0e1017d7abfffbb3826f6bf
ilfpns <yeomsh09@gmail.com>
2026-09-05 17:22:55 +0900

    Add create task func and connect to stack init

--- firmware/src/system/task/task.c
+++ firmware/src/system/task/task.c
@@ -1,1 +1,50 @@
 #include "task.h"
+#include <stdint.h>
+#include <stdio.h>
+
+static TCB_t tcb_pool[MAX_TASK_COUNT];
+uint8_t current_free_slot;
+
+void *stack_init(uint32_t *stack_top, void (*task_enrty)(void)) {
+    *(--stack_top) = 0x10000000;             // xPSR Thumb bit
+    *(--stack_top) = (uint32_t)task_enrty;  // PC
+    *(--stack_top) = 0xFFFFFFFD;            // LR   - EXC_RETURN (PSP, no FPU)
+    *(--stack_top) = 0;                     // R12
+    *(--stack_top) = 0;                     // R3
+    *(--stack_top) = 0;                     // R2
+    *(--stack_top) = 0;                     // R1
+    *(--stack_top) = 0;                     // R0
+
+    *(--stack_top) = 0;                     // R11
+    *(--stack_top) = 0;                     // R10
+    *(--stack_top) = 0;                     // R9
+    *(--stack_top) = 0;                     // R8
+    *(--stack_top) = 0;                     // R7
+    *(--stack_top) = 0;                     // R6
+    *(--stack_top) = 0;                     // R5
+    *(--stack_top) = 0;                     // R4
+
+    return stack_top;
+}
+
+uint8_t create_new_task(char *task_name, uint8_t priority, uint32_t stack_size, uint32_t *stack_mem, void (*stack_entry)(void)) {
+    if (current_free_slot > 10) {
+        printf("Can't create new task \n : Max task limit (%d)", MAX_TASK_COUNT);
+        return 1;
+    }
+
+    TCB_t *tcb = &tcb_pool[current_free_slot++];
+
+    tcb->task_name = task_name;
+    tcb->priority = priority;
+
+    uint32_t *stack_top = stack_mem + stack_size;
+    tcb->sp = stack_init(stack_top, stack_entry);
+
+    if (!((int)tcb->sp & 0x0000000)) {
+        printf("staack init failed");
+        return 1;
+    }
+
+    return 2;
+}
--- firmware/src/system/task/task.h
+++ firmware/src/system/task/task.h
@@ -1,13 +1,23 @@
 #include "stm32f103xb.h"
 #include "stm32f1xx.h"
+#include <stdint.h>
 
-#define STACK_SIZE 256
+#define STACK_SIZE     256
+#define MAX_TASK_COUNT 10
 
 typedef struct {
-    char    task_name[20];
+    char    *task_name;
     uint8_t priority;
     uint8_t state;
     uint8_t *sp;
 } TCB_t;
 
 void *stack_init(uint32_t *stack_top, void (*task_enrty)(void));
+
+/**
+ * @breif Create New task func
+ * @retval
+ *  - 1 : Error
+ *  - 2 : Succes to create new task
+ */
+uint8_t create_new_task(char *task_name, uint8_t priority, uint32_t stack_size, uint32_t *stack_mem, void (*stack_entry)(void));
